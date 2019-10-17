91dbe444625d35dbdc72401ab6e02581af6b014a
Dee <55450806+dpudi596@users.noreply.github.com>
2019-10-17 14:49:59 -0400

    Update api_responce.py

--- api_responce.py
+++ api_responce.py
@@ -1,8 +1,8 @@
 import requests, json
 
-response = requests.get(url = "https://comcast.service-now.com/nav_to.do?uri=%2F$pa_dashboard.do")
+response = requests.get(url = "")
 print(response)
 
 from requests import get
-response1 = get('https://comcast.service-now.com/nav_to.do?uri=%2F$pa_dashboard.do', auth=('dpudi200','Sridevi2121'))
+response1 = get('https://comcast.service-now.com/nav_to.do?uri=%2F$pa_dashboard.do', auth=('',''))
 print(response1)
